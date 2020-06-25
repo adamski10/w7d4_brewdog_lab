61ac7d244c5f977a32631b258d915477e381c2d0
AdamK <adamkoziel@outlook.com>
2020-06-25 15:15:41 +0100

    can remove from favlist

--- src/components/FavouritesList.vue
+++ src/components/FavouritesList.vue
@@ -2,7 +2,8 @@
   <div>
     <h2>Favourites</h2>
     <ul>
-      <li v-for="(fave, key) in favouriteBeers">{{ fave.name }}</li>
+      
+      <li v-for="(fave, key) in favouriteBeers" :key='key'>{{ fave.name }}  <button type="submit" v-on:click="removeFavorite(fave.name)">Remove</button>  </li>
     </ul>
   </div>
 </template>
@@ -18,10 +19,18 @@ export default {
   },
   mounted() {
     eventBus.$on("beer", beer => {
-      if(!this.favouriteBeers.includes(beer)) {
+       console.log('adding beer');
+     if(!this.favouriteBeers.includes(beer)) {
         this.favouriteBeers.push(beer);
       }
+      
     });
+  },
+  methods: {
+    removeFavorite(name) {
+      let beer = this.favouriteBeers.find(beer => beer.name == name);
+       this.favouriteBeers.splice(this.favouriteBeers.indexOf(beer), 1);
+    }
   }
 };
 </script>
